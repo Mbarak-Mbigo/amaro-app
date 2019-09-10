6f9acd17a987469688db039c8ceb77fa9d246750
Mbarak Mbigo <mbarak.mbigo@allangray.co.za>
2019-09-10 14:06:47 +0300

    fix: failing tests  - add travis.yml configuration

--- src/app/__tests__/dashboard.test.jsx
+++ src/app/__tests__/dashboard.test.jsx
@@ -1,12 +1,42 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
+import { BrowserRouter as Router } from "react-router-dom";
+import configureMockStore from "redux-mock-store";
 
 import Dashboard from "../dashboard";
 
-describe("DashBoard Component", () => {
-  const wrapper = shallow(<Dashboard />);
+const mockStore = configureMockStore([]);
 
+describe("DashBoard Component", () => {
+  const store = mockStore({
+    cart: {
+      selected: []
+    },
+    products: {
+      data: [],
+      isLoading: false,
+      hasLoaded: false,
+      error: {
+        hasError: false,
+        error: ''
+      }
+    }
+  });
   test("renders Dashboard component", () => {
-    expect(wrapper).toMatchSnapshot();
+    const component = renderer.create(
+      <Provider store={store}>
+        <Router>
+          <Dashboard />
+        </Router>
+      </Provider>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test("Renders 2 children", () => {
+    const wrapper = shallow(<Dashboard />);
+    expect(wrapper.children()).toHaveLength(2);
   });
 });
